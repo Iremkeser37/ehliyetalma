0b3a8e2708d146452d14b153e3e65e40afefa2bc
Iremkeser37 <145453393+Iremkeser37@users.noreply.github.com>
2023-10-03 15:06:30 +0300

    Add files via upload

---
+++ bilisimm-2/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace bilisimm_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMesaj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMesaj
+            // 
+            this.btnMesaj.Location = new System.Drawing.Point(188, 154);
+            this.btnMesaj.Name = "btnMesaj";
+            this.btnMesaj.Size = new System.Drawing.Size(145, 62);
+            this.btnMesaj.TabIndex = 0;
+            this.btnMesaj.Text = "mesaj var";
+            this.btnMesaj.UseVisualStyleBackColor = true;
+            this.btnMesaj.Click += new System.EventHandler(this.btnMesaj_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 402);
+            this.Controls.Add(this.btnMesaj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMesaj;
+    }
+}
+
---
+++ bilisimm-2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bilisimm_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMesaj_Click(object sender, EventArgs e)
+        {
+            int yas = 14;
+
+            if (yas >= 18)
+            {
+                MessageBox.Show("Ehliyet Alabilirsiniz");
+            }
+            if (yas < 18)
+                MessageBox.Show("Üzgünüm ehliyet yok");
+        }
+    }
+}
+           
